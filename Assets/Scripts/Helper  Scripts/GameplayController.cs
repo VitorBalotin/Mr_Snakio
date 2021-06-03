7ad430bbcdc7165be19ec0815c8aaca801a7b4fb
Vitor <vitorbalotin@gmail.com>
2021-06-03 17:31:36 -0300

    3d-cam

--- Assets/Scripts/Helper  Scripts/GameplayController.cs
+++ Assets/Scripts/Helper  Scripts/GameplayController.cs
@@ -8,9 +8,9 @@ public class GameplayController : MonoBehaviour{
     // pickups
     public GameObject fruit_pickup, bomb_pickup;
     // position x and y from the map
-    private float min_x = -4.25f, max_x = 4.25f, min_y= -2.26f, max_y = 2.26f;
+    private float min_x = -6.49f, max_x = 6.49f, min_z= -2.26f, max_z = 2.26f;
     // z position of the middle of the map
-    private float z_pos = 5.8f;
+    private float y_pos = 0f;
     // Text and counter for the score
     private Text score_text;
     private int score_count;
@@ -43,9 +43,9 @@ public class GameplayController : MonoBehaviour{
     IEnumerator SpawnPickUps(){
         yield return new WaitForSeconds(Random.Range(1f, 1.5f));
         if(Random.Range(0, 10) >= 2){
-            Instantiate(fruit_pickup, new Vector3(Random.Range(min_x, max_x), Random.Range(min_y, max_y), z_pos), Quaternion.identity);
+            Instantiate(fruit_pickup, new Vector3(Random.Range(min_x, max_x), y_pos, Random.Range(min_z, max_z)), Quaternion.identity);
         }else{
-            Instantiate(bomb_pickup, new Vector3(Random.Range(min_x, max_x), Random.Range(min_y, max_y), z_pos), Quaternion.identity);
+            Instantiate(bomb_pickup, new Vector3(Random.Range(min_x, max_x), y_pos, Random.Range(min_z, max_z)), Quaternion.identity);
         }
 
         Invoke("StartSpawning", 0f);
